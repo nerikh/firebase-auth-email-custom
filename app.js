78c08dffee663183d1a3cf22de4d7e1ab7bcda9c
NEH <erik@bodigiti.com>
2017-05-20 14:10:36 -0400

    Through LESSON 18 -- DELETE user.

--- app.js
+++ app.js
@@ -9,6 +9,24 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.bodyParser());
 server.use(restify.queryParser());
 
+// CREATE User
+// take request object, etc..
+// if a 'post' occurs to the /user url endpoint, then execute this code
+server.post("/user", function(req, res, next) {
+  // in our http request, the parameters are going to define our new user
+  var user = req.params;
+  max_user_id++; // increment the max_user_id
+  user.id = max_user_id; // id of new user is current max_user_id (incremented) 
+  /* user array, which consits of all users, the key is "user.id" and 
+   * the data is from "user" (var = user) */
+  users[user.id] = user;
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  // respond with json string consisting of info related to this user
+  res.end(JSON.stringify(user));
+  return next();
+});
+
 // READ Users
 // if root url is requested (an event), run this callback function in nodejs
 // ... 'get' is initiated and completes the function code 
@@ -33,24 +51,6 @@ server.get("/user/:id", function(req, res, next) {
   return next();
 });
 
-// CREATE User
-// take request object, etc..
-// if a 'post' occurs to the /user url endpoint, then execute this code
-server.post("/user", function(req, res, next) {
-  // in our http request, the parameters are going to define our new user
-  var user = req.params;
-  max_user_id++; // increment the max_user_id
-  user.id = max_user_id; // id of new user is current max_user_id (incremented) 
-  /* user array, which consits of all users, the key is "user.id" and 
-   * the data is from "user" (var = user) */
-  users[user.id] = user;
-  res.setHeader('content-type', 'application/json');
-  res.writeHead(200);
-  // respond with json string consisting of info related to this user
-  res.end(JSON.stringify(user));
-  return next();
-});
-
 // UPDATE User
 // if a 'put' to /user/id url
 server.put("/user/:id", function(req, res, next) {
@@ -66,6 +66,15 @@ server.put("/user/:id", function(req, res, next) {
   return next();
 });
 
+// DELETE User
+server.del("/user/:id", function(req, res, next) {
+  delete users[parseInt(req.params.id)];
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  // respond true if user was deleted
+  res.end(JSON.stringify(true));
+  return next();
+});
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
