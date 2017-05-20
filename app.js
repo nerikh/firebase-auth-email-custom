2681f5a895cf52313bd113ec0e6bcbb07ee32674
NEH <erik@bodigiti.com>
2017-05-20 12:16:26 -0400

    First commit at lesson not until lesson 14.

---
+++ app.js
@@ -0,0 +1,39 @@
+var restify = require('restify');
+var server = restify.createServer();
+
+var users = {  };
+var max_user_id = 0;
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.bodyParser());
+
+// if root url is requested (that's an event), run this callback function in nodejs
+// ... 'get' is initiated and completes the function code 
+server.get("/", function(req, res, next) {
+  // tell client that it can expect json to be returned
+  res.setHeader('content-type', 'application/json');
+  // return the header to the client
+  res.writeHead(200);
+  // send data back in the response body, a json response string
+  res.end(JSON.stringify(users));
+  // ensure no further execution takes place after function is executed
+  return next();
+});
+
+// take request object, etc..
+// if a 'post' occurs to the /user url endpoint, then execute this code
+server.post("/user", function(req, res, next) {
+  // in our http request, the parameters are going to define our new user
+  var user = req.params;
+  max_user_id++; // increment the max_user_id
+  user.id = max_user_id; // id of new user is whatever the current max_user_id is (incremented) 
+  users[user.id] = user; // user array, which consits of all users, the key is "user.id" and the data is from "user" (var = user)
+  res.setHeader('content-type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify(user)); // respond with json string consisting of info related to this user
+  return next();
+});
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
