af67f4beb1f519f870833d2299464f41186edbe8
NEH <erik@bodigiti.com>
2017-05-20 21:59:28 -0400

    Through LESSON 29 - Using API Keys with Basic Auth.

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -7,5 +7,25 @@ module.exports = function(server, restify, restifyValidator) {
   server.use(restify.bodyParser());
   server.use(restify.queryParser());
   server.use(restifyValidator);
+
+  // API Authorization
+  server.use(restify.authorizationParser());
+  server.use(function(req, res, next) {
+    var apiKeys = {
+      // user : password
+      'user1': 'akfjrekjrakfj328u04328u'
+    };
+    if (typeof(req.authorization.basic) === 'undefined' || !apiKeys[req.authorization.basic.username] || req.authorization.basic.password !== apiKeys[req.authorization.basic.username] ) {
+      var response = {
+        'status': 'failure',
+        'data': 'You must specify a valid API key'
+      };
+      res.setHeader('content-type', 'application/json');
+      res.writeHead(403); // response code 403 = forbidden 
+      res.end(JSON.stringify(response));
+      return next();
+    }
+    return next();
+  });
 }
 
